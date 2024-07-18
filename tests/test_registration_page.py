2cc65ced58abd80d97f4ae94b8abc1cd59386ae0
Anastasiia Strelnik <n.strelnik@mail.ru>
2024-07-18 08:55:17 +0200

    заполнение формы

--- tests/test_registration_page.py
+++ tests/test_registration_page.py
@@ -6,12 +6,12 @@ def test_student_registration_form():
     registration_page.open()
     (
         registration_page
-        .fill_first_name('Masha')
-        .fill_last_name('Www')
+        .fill_first_name('Anastasiia')
+        .fill_last_name('Strelnik')
         .fill_user_email('test@gmail.com')
         .fill_user_gender('Female')
-        .fill_user_number('1234456789')
-        .fill_day_of_birth('1999', 'May', '11')
+        .fill_user_number('9216666666')
+        .fill_day_of_birth('2001', 'March', '29')
         .fill_user_subject('Maths')
         .fill_user_hobby('Sports')
         .fill_user_picture('picture.jpg')
@@ -20,6 +20,6 @@ def test_student_registration_form():
         .fill_user_city('Delhi')
         .click_submit()
     )
-    registration_page.registered_user_with('Masha Www', 'test@gmail.com', 'Female', '1234456789', '11 May,1999',
+    registration_page.registered_user_with('Anastasiia Strelnik', 'test@gmail.com', 'Female', '9216666666', '29 March,2001',
                                              'Maths', 'Sports', 'picture.jpg',
                                              'Quitzon Common, South Kraigville', 'NCR Delhi')
