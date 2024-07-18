3adc7160945906c2466be5620b7c6301e56f131d
Anastasiia Strelnik <n.strelnik@mail.ru>
2024-07-18 08:52:13 +0200

    Размер окна браузера

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@ from selene import browser
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1366
-    browser.config.window_height = 768
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
     yield
 
