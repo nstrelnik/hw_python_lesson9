c7a3bee98fb9fc1f8b389e6b136830a378ed7b88
Anastasiia Strelnik <n.strelnik@mail.ru>
2024-07-19 09:30:44 +0200

    init

--- tests/test_registration_page.py
+++ tests/test_registration_page.py
@@ -15,7 +15,7 @@ def test_student_registration_form():
         .fill_user_subject('Maths')
         .fill_user_hobby('Sports')
         .fill_user_picture('picture.jpg')
-        .fill_user_current_address('Quitzon Common, South Kraigville')
+        .fill_user_current_address('Kaliningrad')
         .fill_user_state('NCR')
         .fill_user_city('Delhi')
         .click_submit()
